39ef6a20f4822bf86fa98f4c0c11450d76f21521
Pasquale Abate <lino.abate@gmail.com>
2024-08-01 18:08:37 +0200

    fix form contatct

--- main.js
+++ main.js
@@ -29,3 +29,38 @@ input.forEach(element => {
 /* Svuota textarea */
 let textarea = document.querySelector('textarea');
 textarea.value = '' ;
+
+/* web3Form */
+
+document.getElementById('contact-form').addEventListener('submit', async function(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+
+  formData.append("access_key", "d9e470c5-9f3a-4413-99ec-95dab6ea193c");
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          },
+          body: json
+      });
+
+      const result = await response.json();
+      if (result.success) {
+          console.log(result);
+          // Puoi aggiungere qui un messaggio di successo per l'utente
+      } else {
+          console.error(result);
+          // Puoi aggiungere qui un messaggio di errore per l'utente
+      }
+  } catch (error) {
+      console.error('Error:', error);
+      // Puoi aggiungere qui un messaggio di errore per l'utente
+  }
+});
